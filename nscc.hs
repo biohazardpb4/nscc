e55176bc7f09738e88f755843a4f776a6a896895
biohazardpb4 <jay@jprodevelopment.com>
2018-06-08 23:08:51 -0500

    we have a compiler which can return arbitrary exit codes!

--- nscc.hs
+++ nscc.hs
@@ -1,14 +1,21 @@
 import System.Environment
 import System.IO
-  
+import Text.Regex
+ 
 main = do
   args <- getArgs
   let cFile = (head args)
-  let assFile = (take ((length cFile) - 1) cFile) ++ "s"
-  writeFile assFile ("\    
-	\    .globl main\n\
-	\main:\n\
-	\    movl    $"++ show 2 ++ ", %eax\n\
-	\    ret\n\
-	\")
+
+  cContents <- readFile cFile
+  let maybeRet = matchRegex (mkRegex "return ([0-9]+);") cContents
+  case maybeRet of
+    Nothing -> error $ "number not found"
+    Just ret -> do 
+      let assFile = (take ((length cFile) - 1) cFile) ++ "s"
+      writeFile assFile ("\    
+		\    .globl main\n\
+		\main:\n\
+		\    movl    $"++ (head ret) ++ ", %eax\n\
+		\    ret\n\
+		\")
 
